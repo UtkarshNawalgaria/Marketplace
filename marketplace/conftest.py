6c301cb6aa38089e11d2d3581438832caf158831
Utkarsh Nawalgaria <utkarsh.n@olivecloud.in>
2022-04-29 22:14:39 +0530

    removed tests, pytest action

--- marketplace/conftest.py
+++ marketplace/conftest.py
@@ -1,14 +1,6 @@
 import pytest
 
-from marketplace.users.models import User
-from marketplace.users.tests.factories import UserFactory
-
 
 @pytest.fixture(autouse=True)
 def media_storage(settings, tmpdir):
     settings.MEDIA_ROOT = tmpdir.strpath
-
-
-@pytest.fixture
-def user() -> User:
-    return UserFactory()
